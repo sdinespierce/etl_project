38ea0633ea94f77809a6c0ac0d1c58b59ebb9f85
Terrell Bradford <terrell.bradford@gmail.com>
2021-08-08 21:55:41 -0400

    Populated dict.html

--- scripts/dictionary_to_html.py
+++ scripts/dictionary_to_html.py
@@ -1,11 +1,11 @@
 import pandas as pd
  
 # to read csv file named "samplee"
-a = pd.read_csv("ETL_Project_Data_Dictionary.csv")
+a = pd.read_csv("../Report/Data_Dictionary.csv", encoding='latin1')
  
 # to save as html file
 # named as "Table"
-a.to_html("ETL_Project_Data_Dictionary.html")
+a.to_html("ETL_Project_Data_Dictionary.html", index=False)
  
 # assign it to a
 # variable (string)
